f151e312a75ef48bf11aa9473fd01b9fc4381a22
zhangyugehu <1021091945@qq.com>
2017-01-10 17:54:56 +0800

    数据分离
    将模拟数据分离到单独js文件中

---
+++ data/post-data.js
@@ -0,0 +1,47 @@
+var local_database = {
+    "post_key": [
+        {
+            date: "Sep 18 2016",
+            title: "正是虾肥蟹壮时",
+            img: {
+                imgSrc: "/images/post/banner.jpg",
+                avatar: "/images/avater.jpg",
+                headImgSrc: "/images/avater.jpg",
+            },
+            hasImage: true,
+            content: "菊黄蟹正肥，品尝秋之味。徐志摩把,“看初花的荻芦”和“到楼外楼吃蟹”,并列为秋天来杭州不能错过的风雅之事；用林妹妹的话讲是“螯封嫩玉双双满，",
+            reading: "112",
+            collection: "96",
+        },
+        {
+            date: "Nov 20 2016",
+            title: "比利·林恩的中场故事",
+            img: {
+                imgSrc: "/images/post/banner.jpg",
+                avatar: "/images/avater.jpg",
+                headImgSrc: "/images/avater.jpg",
+            },
+            hasImage: false,
+            content: "一 “李安是一位绝不会重复自己的导演，本片将极富原创性李安众所瞩目的新片《比利林恩漫长的中场休息》，正式更名《半场无战事》。",
+            reading: "62",
+            collection: "92",
+        },
+        {
+            date: "Sep 18 2016",
+            title: "正是虾肥蟹壮时",
+            img: {
+                imgSrc: "/images/post/banner.jpg",
+                avatar: "/images/avater.jpg",
+                headImgSrc: "/images/avater.jpg",
+            },
+            hasImage: true,
+            content: "菊黄蟹正肥，品尝秋之味。徐志摩把,“看初花的荻芦”和“到楼外楼吃蟹”,并列为秋天来杭州不能错过的风雅之事；用林妹妹的话讲是“螯封嫩玉双双满，",
+            reading: "112",
+            collection: "96",
+        }
+    ]
+}
+
+module.exports={
+    postList:local_database
+}
--- pages/posts/post.js
+++ pages/posts/post.js
@@ -1,4 +1,4 @@
-// var postsData = require('../../data/post-data.js')
+var postsData = require('../../data/post-data.js')
 
 Page({
   data:{
@@ -8,48 +8,7 @@ Page({
   },
   onLoad:function(options){
     // 生命周期函数--监听页面加载
-      var posts_content = {"post_key":[
-          {
-              date: "Sep 18 2016",
-              title: "正是虾肥蟹壮时",
-              img: {
-                  imgSrc: "/images/post/banner.jpg",
-                  avatar: "/images/avater.jpg",
-                  headImgSrc: "/images/avater.jpg",
-              },
-              hasImage: true,
-              content: "菊黄蟹正肥，品尝秋之味。徐志摩把,“看初花的荻芦”和“到楼外楼吃蟹”,并列为秋天来杭州不能错过的风雅之事；用林妹妹的话讲是“螯封嫩玉双双满，",
-              reading: "112",
-              collection: "96",
-          },
-          {
-              date: "Nov 20 2016",
-              title: "比利·林恩的中场故事",
-              img: {
-                  imgSrc: "/images/post/banner.jpg",
-                  avatar: "/images/avater.jpg",
-                  headImgSrc: "/images/avater.jpg",
-              },
-              hasImage: false,
-              content: "一 “李安是一位绝不会重复自己的导演，本片将极富原创性李安众所瞩目的新片《比利林恩漫长的中场休息》，正式更名《半场无战事》。",
-              reading: "62",
-              collection: "92",
-          },
-          {
-              date: "Sep 18 2016",
-              title: "正是虾肥蟹壮时",
-              img: {
-                  imgSrc: "/images/post/banner.jpg",
-                  avatar: "/images/avater.jpg",
-                  headImgSrc: "/images/avater.jpg",
-              },
-              hasImage: true,
-              content: "菊黄蟹正肥，品尝秋之味。徐志摩把,“看初花的荻芦”和“到楼外楼吃蟹”,并列为秋天来杭州不能错过的风雅之事；用林妹妹的话讲是“螯封嫩玉双双满，",
-              reading: "112",
-              collection: "96",
-          }
-      ]};
-    this.setData(posts_content);
+    this.setData(postsData.postList);
   },
   onReady:function(){
     // 生命周期函数--监听页面初次渲染完成
